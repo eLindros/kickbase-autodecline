9c47cfdd6c86b37ae77eaeaad66d9a38268e08e8
Ronny Klein <ronny.klein@me.com>
2022-10-12 22:02:58 +0200

    refactor into typescript classes

--- netlify/functions/kickbase-autodecline.ts
+++ netlify/functions/kickbase-autodecline.ts
@@ -1,31 +1,7 @@
-import { Handler } from "@netlify/functions";
-import axios from "axios";
-import { KickbaseLogin } from "../classes/Login";
+import { Handler } from '@netlify/functions';
+import { KickbaseApi } from '../api/KickbaseApi';
 
 // Login to Kickbase => Token
-const getFirstLeagueId = async (): Promise<string> => {
-  return axios({
-    url: "https://api.kickbase.com/leagues",
-    method: "GET",
-  }).then(
-    (response) => {
-      if (response.status === 200) {
-        if (
-          response.data &&
-          response.data.leagues &&
-          response.data.leagues.length
-        ) {
-          return response.data.leagues[0].id || "no league id";
-        } else {
-          return "no league";
-        }
-      } else {
-        return "Error. Response status: " + response.status;
-      }
-    },
-    (error) => "Error: " + error
-  );
-};
 
 // Get all players with market values and offers
 
@@ -36,22 +12,15 @@ const getFirstLeagueId = async (): Promise<string> => {
 // every hour: remove player with too low offer from market
 
 const handler: Handler = async () => {
+  const user: string = process.env.KICKBASE_USER || 'none';
+  const password: string = process.env.KICKBASE_PASSWORD || 'none';
 
-const user: string = process.env.KICKBASE_USER || 'none';
-const password: string = process.env.KICKBASE_PASSWORD || 'none';
-
-  const login = new KickbaseLogin(
-    "https://api.kickbase.com/user/login",
-    user,
-    password,
-  );
-  await login.login();
-  const Token = login.token;
-  axios.defaults.headers.common["Authorization"] = Token;
-  console.log(await getFirstLeagueId());
+  const kickbase = new KickbaseApi(user, password);
+  await kickbase.getLeagues();
+  if (kickbase.data.leagues) console.log(kickbase.data.leagues);
 
   return {
-    statusCode: 300,
+    statusCode: 200,
   };
 };
 
