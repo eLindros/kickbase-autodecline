25d40293c6b8679a5b249c7843d029c52bbad87f
Ronny Klein <ronny.klein@me.com>
2022-10-21 07:43:27 +0000

    added node-cron

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import './config';
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
+import cron from 'node-cron';
 import { autodecline } from './kickbase-autodecline';
 
 const app = express();
@@ -15,5 +16,12 @@ app.get('/', async (_req: Request, res: Response) => {
 });
 
 var port = process.env.PORT || '3000';
+
+
+cron.schedule("*/15 * * * * *", function () {
+  console.log("---------------------");
+  console.log("running a task every 15 seconds");
+});
+
 app.set('port', port);
 app.listen(port);
