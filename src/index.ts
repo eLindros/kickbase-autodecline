97ebcb232c081c9cc0ed22479202633d27c461e0
Ronny Klein <ronny.klein@me.com>
2022-10-22 20:30:31 +0200

    test node-schedule to improve memory leak

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import './config';
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
-import cron from 'node-cron';
+// import cron from 'node-cron';
+import schedule from 'node-schedule';
 import { autodecline } from './kickbase-autodecline';
 
 const app = express();
@@ -17,16 +18,21 @@ app.get('/', async (_req: Request, res: Response) => {
 
 var port = process.env.PORT || '3000';
 
-cron.schedule('0 0-20,23 * * *', function () {
+const rule = new schedule.RecurrenceRule();
+rule.minute = 0;
+rule.hour = [new schedule.Range(0, 20), 23];
+rule.tz = 'Europe/Berlin';
+
+const job = schedule.scheduleJob(rule, function () {
+  // cron.schedule('0 0-20,23 * * *', function () {
   console.log('---------------------');
   console.log('running a task: 0 23-20 * * *');
   console.log(new Date());
-},
-{
-   scheduled: true,
-	 timezone: "Europe/Berlin"
- }
-);
+  // },
+  // {
+  //    scheduled: true,
+  // 	 timezone: "Europe/Berlin"
+});
 
 app.set('port', port);
 app.listen(port);
